7781bbaf580dbc0ec81eddb46af45b80592c6bd0
Santiago Quintero <santiago.qp86@gmail.com>
2023-11-17 15:32:20 -0500

    17 nov 2023

---
+++ js/vertex_shader.js
@@ -0,0 +1,10 @@
+export const vertexCode = `
+    attribute vec3 coordinates;
+    varying vec2 vTextureCoord;
+    
+    void main(void) {
+        vTextureCoord = coordinates.xy;
+
+
+        gl_Position = vec4(coordinates, 1.0);
+    }`;
